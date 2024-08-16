c211f69ac9af3340d6b60a20d4837311eb820d12
viraj naik <vn3246786@gmail.com>
2024-08-16 11:02:12 +0530

    update

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,8 +4,9 @@ import App from './App.jsx'
 import UserContextProvider from './Contexts/UserContext/UserContext.jsx'
 import WatchListContextProvider from './Contexts/WatchListContext/WatchListContext.jsx'
 import AccessTokenContextProvider from './Contexts/AccessTokenContext/AccessTokenContext.jsx'
+import axios from 'axios'
 
-
+axios.defaults.withCredentials=true
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <UserContextProvider>
