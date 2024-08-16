e08eb416428f7d425e4b7fe1d3bd1729a1119d0b
viraj naik <vn3246786@gmail.com>
2024-08-16 12:06:44 +0530

    update

--- api/VerifyToken.js
+++ api/VerifyToken.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const verify = (req , res , next) => {
+    console.log(req.headers.token)
     const authToken = req.headers.token
     if(authToken){
         const token = authToken.split(" ")[1]
