b61448129e87aad6d5df0658c79790f4ad6535fa
viraj naik <vn3246786@gmail.com>
2024-08-17 19:24:54 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config()
 const express = require('express');
+const cors = require("cors")
 const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
