cb2aa15cbb75ea994895b4922ccb27f44f5d68d8
viraj naik <vn3246786@gmail.com>
2024-08-17 20:37:43 +0530

    update

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const cookie =require('cookie-parser')
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-type, token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next()
 })
