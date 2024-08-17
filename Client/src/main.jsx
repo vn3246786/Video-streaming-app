23bb6ae19150f58c571c24b062465d7519b23d7f
viraj naik <vn3246786@gmail.com>
2024-08-17 10:23:10 +0530

    update

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,7 +6,6 @@ import WatchListContextProvider from './Contexts/WatchListContext/WatchListConte
 import AccessTokenContextProvider from './Contexts/AccessTokenContext/AccessTokenContext.jsx'
 import axios from 'axios'
 
-axios.defaults.withCredentials=true
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
