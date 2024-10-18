e73b0ed5ffd96703cf7f8b15185496c472c38d22
viraj naik <vn3246786@gmail.com>
2024-10-18 07:08:30 +0530

    update

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-   
-proxy:{
-  "/api":"https://video-streaming-app-dguh.onrender.com",
-  
-}
-  },
   plugins: [react()],
 })
--- admin panel/vite.config.js
+++ admin panel/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- server:{
-    proxy:{
-      "/api":"https://video-streaming-app-6wzt.onrender.com"
-    }
-  }, plugins: [react()]
+ plugins: [react()]
 })
